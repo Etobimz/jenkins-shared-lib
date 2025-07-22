6f9bbf93f6f8a4833eb2de52dfea73a68447e78f
Etobimz <etooluwa200@gmail.com>
2025-07-22 13:33:31 -0600

    missing contents

---
+++ vars/buildImage.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+
+def call () {
+
+ echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId:'docker-gub-maven-rep', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD' )])
+        {
+            sh '''
+            echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+            docker build -t java-maven-app:2.0 .
+            docker tag java-maven-app:2.0 $DOCKER_USERNAME/java-maven-app:2.0
+            docker push $DOCKER_USERNAME/java-maven-app:2.0
+
+            '''
+         }
+
+
+
+}
---
+++ vars/buildJar.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call () {
+
+     echo "Building the application..."
+                    sh 'mvn package'
+
+
+}
